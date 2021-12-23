46bbefbf392c398c831fce82ad26cc99f9890d12
Joseph Choi <joseph2173@hotmail.com>
2021-12-23 16:50:58 +0800

    move to src

--- src/sample_generator.py
+++ src/sample_generator.py
@@ -1,14 +1,14 @@
-import pandas as pd 
-import numpy as np 
-from PIL import Image
-import os 
+import os
 
-def gen_csv(root = "../tile_masks"):
+from PIL import Image
+import numpy as np
+import pandas as pd 
+
+def gen_csv(root = "../tile_masks", verbose = 0):
     df = pd.DataFrame(columns=["tile_name", "case_index", "label_0", "label_1", "label_2"])
     for case in os.listdir(root):
         case_ind = case
         npy = "/npy"
-        masks = "/masks"
         tiles = "/tiles"
         for tile in os.listdir(root + '/' + case + tiles):
             tile_name = tile.removesuffix('.png')
@@ -18,18 +18,18 @@ def gen_csv(root = "../tile_masks"):
             label_1 = d[1]/sum(d.values())
             label_2 = d[2]/sum(d.values())
             df = df.append({'tile_name': tile_name, "case_index":case_ind, "label_0":label_0, "label_1":label_1, "label_2": label_2}, ignore_index=True)
-            print(tile_name)
+            if verbose:
+                print(tile_name)
         
-    print(df)
     df.to_csv("tile_masks.csv", index=False)
-    return(df)
+    return df
 
-def gen_png(root = "../tile_masks",df = pd.read_csv("tile_masks.csv"), sample_size = 100, out_dir="sample"):
+def gen_png(df, root = "../tile_masks", sample_size = 100, out_dir="sample", rule_0 = 0.99, rule_1 = 0.5, rule_2 = 0.1):
 
     df["label"] = np.nan
-    df.loc[df.label_0>0.99, 'label'] = 0
-    df.loc[df.label_1>0.5, 'label'] = 1
-    df.loc[df.label_2>0.1, 'label'] = 2
+    df.loc[df.label_0>rule_0, 'label'] = 0
+    df.loc[df.label_1>rule_1, 'label'] = 1
+    df.loc[df.label_2>rule_2, 'label'] = 2
     df_sample = pd.DataFrame()
 
     for i in range(3):
@@ -47,13 +47,10 @@ def gen_png(root = "../tile_masks",df = pd.read_csv("tile_masks.csv"), sample_si
         df_sample = pd.concat([df_sample,rd])
     df_sample.to_csv(out_dir + "/" + out_dir + ".csv", index = False)
 
-    pass
-
-
 def main():
     # df = gen_csv(root = "../tile_masks")
-    gen_png(root = "../tile_masks",df = pd.read_csv("tile_masks.csv"),sample_size = 100)
-    pass
+    df = pd.read_csv("tile_mask.csv")
+    gen_png(df = df, sample_size = 100)
+
 if __name__ == "__main__":
     main()
-    pass
